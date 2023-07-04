44541dad3753613ebf7af8fc63ff8b475ff2065c
BeastCoder99 <138277505+BeastCoder99@users.noreply.github.com>
2023-07-04 10:21:48 +0300

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,23 @@
+import{
+View,
+Text,
+Stylesheet,
+SafeAreaView,
+TouchableOpacity,
+Platform,
+StatusBar,
+ImageBackground,
+Image
+} 
+from "react-native";
+
+<ImageBackground>"space.gif"</ImageBackground>
+
+<TouchableOpacity style = {style.routeCard} onPress= {()=>
+this.props.navigation.navigate("Tonight Sky")
+
+
+}
+
+
+>
